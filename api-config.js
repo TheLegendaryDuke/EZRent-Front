cbc11490520bfa533d5c1ae292063ea8708e82bc
Aaron <454048573@qq.com>
2019-01-11 16:02:01 -0500

    updated backend host

--- api-config.js
+++ api-config.js
@@ -5,7 +5,7 @@ const hostname = window && window.location && window.location.hostname;
 if(hostname === 'ezrent.aoranzhang.com') {
     backendHost = 'https://ezrent.aoranzhang.com/api';
 } else {
-    backendHost = 'http://' + hostname + ':8443';
+    backendHost = 'http://' + hostname + ':8443/api';
 }
 
 export const API_ROOT = `${backendHost}/graphql`;
